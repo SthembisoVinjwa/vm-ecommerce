79cd413e2ea32ea7637624eab4f452f5fede2d5b
23745444 <23745444@sun.ac.za>
2024-01-08 23:16:50 +0200

    add route

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
-app.get('/', (req, res, next) => {
-    return res.status(200).json({
-        message: "Home page"
+const usersRouter = require('./routes/users')
+
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.use(morgan('tiny'))
+
+app.use('/users', usersRouter)
+
+app.use('', (req, res, next) => {
+    const error = new Error('Not found')
+    res.status(404)
+    next(error)
+})
+
+app.use((err, req, res, next) => {
+    res.json({
+        error: err
     })
 })
 
