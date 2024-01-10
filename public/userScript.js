adf16025ef7c7e8b4175271398268e9f8742ac71
23745444 <23745444@sun.ac.za>
2024-01-10 14:23:10 +0200

    Password updates

--- public/userScript.js
+++ public/userScript.js
@@ -45,7 +45,7 @@ function signIn (email, password) {
       response.json().then(data => {
         if (status === 200) {
           setCookie('access_token', data.token, 30)
-          window.location.assign('/')
+          window.location.assign('/app')
         } else {
           alert(data.message)
         }
@@ -88,46 +88,100 @@ function signUp () {
 function updateUser () {
   let newName = document.getElementById('name-signup').value
   let newEmail = document.getElementById('email-update').value
+  let oldPass = document.getElementById('password-update').value
+  let newPass = document.getElementById('confirm-signup').value
+  let confirmPass = document.getElementById('confirm-update').value
 
   let access_token = document.cookie.split('=')[1]
 
-  if (currentName !== newName || currentEmail !== newEmail) {
-    fetch(server_url + 'users/', {
-      method: 'PATCH',
+  if (oldPass.length > 0 && newPass.length > 0 && confirmPass.length > 0) {
+    if (oldPass === newPass) {
+      return alert('Old Password cannot be used as a new Password')
+    }
+
+    if (newPass !== confirmPass) {
+      return alert('Passwords do not match')
+    }
+    
+    fetch(server_url + 'users/signin', {
+      method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
-        token: 'Bearer ' + access_token
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify([
-        {
-          propName: 'name',
-          propValue: newName
-        },
-        {
-          propName: 'email',
-          propValue: newEmail
-        }
-      ])
-    })
-      .then(response => {
-        alert('Updated successfully')
+      body: JSON.stringify({
+        email: newEmail,
+        password: oldPass
       })
+    }).then(response => {
+      if (response.status === 200) {
+        fetch(server_url + 'users/', {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+            token: 'Bearer ' + access_token
+          },
+          body: JSON.stringify([
+            {
+              propName: 'name',
+              propValue: newName
+            },
+            {
+              propName: 'password',
+              propValue: newPass
+            },
+          ])
+        })
+        .then(
+          alert('Password updated successfully')
+        )
+        .catch(err => {
+          alert(err)
+        })
+      }
+    })
+  } else {
+    if (currentName !== newName || currentEmail !== newEmail) {
+      fetch(server_url + 'users/', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          token: 'Bearer ' + access_token
+        },
+        body: JSON.stringify([
+          {
+            propName: 'name',
+            propValue: newName
+          },
+        ])
+      })
+      .then(alert('Name updated successfully'))
       .catch(err => {
         alert(err)
       })
-  } else {
-    alert('Nothing to update')
+    } else {
+      alert('Nothing to update')
+    }
   }
 }
 
-function newPassword() {
-  let oldPass = document.getElementById("password-update")
-  let newPass = document.getElementById("confirm-signup")
-  let confirmPass = document.getElementById("confirm-update")
+function newPassword () {
+  let oldPass = document.getElementById('password-update')
+  let newPass = document.getElementById('confirm-signup')
+  let confirmPass = document.getElementById('confirm-update')
 
-  oldPass.style.display = "flex"
-  newPass.style.display = "flex"
-  confirmPass.style.display = "flex"
+  let updateBtn = document.getElementById('update-btn')
+
+  if (oldPass.style.display === 'none') {
+    oldPass.style.display = 'flex'
+    newPass.style.display = 'flex'
+    confirmPass.style.display = 'flex'
+    updateBtn.value = 'Keep Password'
+  } else {
+    oldPass.style.display = 'none'
+    newPass.style.display = 'none'
+    confirmPass.style.display = 'none'
+    updateBtn.value = 'Change Password'
+  }
 }
 
 function setCookie (cName, cValue, expDays) {
