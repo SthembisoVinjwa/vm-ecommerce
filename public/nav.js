206054ad67570dfa6fc1f7b4536a87e96a461f46
23745444 <23745444@sun.ac.za>
2024-01-19 09:02:31 +0200

    Problems fixed and files separated

--- public/nav.js
+++ public/nav.js
@@ -1,24 +1,36 @@
 // Login/Logout
-let loginButton = document.getElementById("login-logout")
+let loginButton = document.getElementById('login-logout')
 
 let access_token = document.cookie.split('=')[1]
 
-if (access_token.length > 0) {
-    loginButton.innerText = "Logout"
+if (access_token && access_token.length > 0) {
+  loginButton.innerText = 'Logout'
 } else {
-    loginButton.innerText = "Login"
+  loginButton.innerText = 'Login'
 }
 
-loginButton.addEventListener('mouseover', function() {
-    loginButton.style.cursor = "pointer"
+loginButton.addEventListener('mouseover', function () {
+  loginButton.style.cursor = 'pointer'
 })
 
-loginButton.addEventListener('click', function() {
-    if (loginButton.innerText === 'Login') {
-        window.location.assign("/app/user/signin")
-    } else {
-        document.cookie = 'access_token=' + '; path=/'
-        loginButton.innerText = "Login"
-        alert('Logged out')
-    }   
+loginButton.addEventListener('click', function () {
+  if (loginButton.innerText === 'Login') {
+    window.location.assign('/app/user/signin')
+  } else {
+    document.cookie = 'access_token=' + '; path=/'
+    loginButton.innerText = 'Login'
+    alert('Logged out')
+  }
 })
+
+// Hamburger
+let hamburger = document.getElementById('hamburger-icon')
+
+
+if (hamburger) {
+  hamburger.addEventListener('mouseover', function (){
+    hamburger.style.cursor = 'pointer'
+  })
+}
+
+
